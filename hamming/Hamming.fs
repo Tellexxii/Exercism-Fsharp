7544202e209fb8bfef60287427577dcc09dbd084
Tellexxii <tellexxii@gmail.com>
2021-02-24 18:36:51 +0500

    init

---
+++ hamming/Hamming.fs
@@ -0,0 +1,7 @@
+﻿module Hamming
+
+
+let listComparer list1 list2 = Array.map2 (fun x y -> x = y) list1 list2
+let booleans = listComparer ("AAB".ToCharArray()) ("ABB".ToCharArray())
+
+//let distance (strand1: string) (strand2: string): int option = failwith "You need to implement this function."
